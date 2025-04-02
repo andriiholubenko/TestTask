c4670c231ac62e7cd6db4abe2f9f29c637142fbc
AndriiH <103764137+andriiholubenko@users.noreply.github.com>
2025-04-02 17:31:56 +0300

    Task/readme (#6)

---
+++ README.md
@@ -0,0 +1,21 @@
+## Resources
+This test case represents the creation of a virtual machine in the Azure cloud:
+backend = Azure Blob (created via the Azure portal)
+azure access = Azure Service Principal (created via the Azure portal)
+pub connection key = key vault
+modules = VM, Network, RG, Key Vault
+
+
+## Connection
+After creating all the resources, need to run the following commands to connect to the virtual machine:
+az login
+terraform output -raw vm_private_key > key.pem
+chmod 600 key.pem
+terraform output - raw public_ip_address
+
+## Security rules
+A security rule was created for port 9100 to allow third-party monitoring services.
+SSH connection port 22
+
+## CI/CD
+A CI/CD process was also implemented to automatically deploy resources across environments when merging PRs.
--- terraform-azure/modules/linux-vm/variables.tf
+++ terraform-azure/modules/linux-vm/variables.tf
@@ -1,36 +1,36 @@
 variable "name" {
   type        = string
-  description = "The name of the virtual machine (also used for NIC, public IP, and NSG)"
+  description = "The name of the virtual machine"
 }
 
 variable "resource_group_name" {
   type        = string
-  description = "The name of the resource group in which all resources will be created"
+  description = "The name of the resource group"
 }
 
 variable "location" {
   type        = string
-  description = "Azure region for resource deployment (e.g., westeurope)"
+  description = "Azure region"
 }
 
 variable "subnet_id" {
   type        = string
-  description = "The ID of the subnet to which the virtual machine will be connected"
+  description = "The ID of the subnet"
 }
 
 variable "vm_size" {
   type        = string
   default     = "Standard_B1s"
-  description = "The size/sku of the virtual machine"
+  description = "The sku of the virtual machine"
 }
 
 variable "admin_username" {
   type        = string
   default     = "azureuser"
-  description = "Admin username used to log in to the VM"
+  description = "Admin username"
 }
 
 variable "key_vault_id" {
   type        = string
-  description = "Azure Key Vault ID where the private key will be stored"
+  description = "Azure Key Vault ID"
 }
