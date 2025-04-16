595453c4f58bba129f760bbd651a5d373b29492d
AndriiH <103764137+andriiholubenko@users.noreply.github.com>
2025-04-16 08:50:41 +0300

    feat: add Prometheus and Grafana via Ansible (#8)

--- terraform-azure/envs/prod/variables.tf
+++ terraform-azure/envs/prod/variables.tf
@@ -13,3 +13,15 @@ variable "tenant_id" {
 variable "object_id" {
   type = string
 }
+
+variable "vm_name" {
+  type        = string
+  default     = "prod-linux-vm"
+  description = "Name of the prod VM"
+}
+
+variable "vm_monitoring_name" {
+  type        = string
+  default     = "vm-monitoring"
+  description = "Name of the monitoring VM"
+}
