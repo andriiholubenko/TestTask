aa583465981d8f06965badc2e1c4601e5527e108
AndriiH <103764137+andriiholubenko@users.noreply.github.com>
2025-05-07 11:52:00 +0300

    feat: create aks (#10)

---
+++ aks-cluster/modules/aks/variables.tf
@@ -0,0 +1,4 @@
+variable "cluster_name" { type = string }
+variable "location" { type = string }
+variable "resource_group_name" { type = string }
+variable "node_count" { type = number }
---
+++ aks-cluster/terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name = "aks-rg"
+cluster_name        = "aks-cluster"
+location            = "westeurope"
+node_count          = 2
